6d898b1745da76aa9d38e8c8b559f1e84aa92a9d
Unnati Pathak <unnatipathak78@gmail.com>
2022-07-03 07:11:08 +0530

    June-27-Session

---
+++ index.js
@@ -0,0 +1,49 @@
+//console.log("From the File");
+//console.log("Another");
+//console.log("ABCDEF");
+
+/*let number=40;
+if(number>=0){
+    console.log("number is positive");
+}
+else{
+    console.log("number is negative");
+}*/
+
+/*let number=10;
+if(number==10){
+    console.log("Number is ="+number);
+}*/
+
+let number1=60;
+let number2=20;
+let number3=100;
+if(number1>number2&&number1>number3){
+    console.log("This number is greater "+number1);
+}
+else if(number2>number1&&number2>number3){
+    console.log("This number is greater "+number2);
+}
+else{
+    console.log("This number is greater "+number3);
+}
+
+let marks=89;
+if(marks>=80){
+    console.log("Outstanding");
+}
+else if(marks>=70){
+    console.log("Merit");
+}
+else if(marks>=60){
+    console.log("First Class");
+}
+else if(marks>=50){
+    console.log("Second Class");
+}
+else if(marks>=35){
+    console.log("Pass");
+}
+else{
+    console.log("Fail");
+}
